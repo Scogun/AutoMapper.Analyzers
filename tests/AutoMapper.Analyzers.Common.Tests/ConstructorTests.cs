59b8e7427bb22e6e63e825a50556ca3fde3afab8
Sergey <SAntonov@UCASoft.com>
2022-05-26 00:14:32 +0200

    Add Build and Test action (#3)

--- tests/AutoMapper.Analyzers.Common.Tests/ConstructorTests.cs
+++ tests/AutoMapper.Analyzers.Common.Tests/ConstructorTests.cs
@@ -4,9 +4,9 @@ namespace AutoMapper.Analyzers.Common.Tests;
 
 public class ConstructorTests : BaseAnalyzerTests
 {
-    [TestCase("{|#0:public TestProfile() { }|}")]
-    [TestCase("{|#0:public TestProfile() { Console.WriteLine(); }|}")]
-    [TestCase("{|#0:public TestProfile() { if (true) {} }|}")]
+    [TestCase("{|#0:public TestProfile() { }|}", TestName = "Empty Profile Constructor")]
+    [TestCase("{|#0:public TestProfile() { Console.WriteLine(); }|}", TestName = "Profile Constructor with only extra invocation")]
+    [TestCase("{|#0:public TestProfile() { if (true) {} }|}", TestName = "Profile Constructor with only extra statement")]
     public async Task ConstructorAnalyzerRaiseDiagnostic(string constructorBody)
     {
         var expected = AnalyzerVerifier<ConstructorAnalyzer>.Diagnostic(ConstructorAnalyzer.DiagnosticId).WithArguments("TestProfile").WithLocation(0);
--- tests/AutoMapper.Analyzers.Common.Tests/MapFromTests.cs
+++ tests/AutoMapper.Analyzers.Common.Tests/MapFromTests.cs
@@ -4,30 +4,30 @@ namespace AutoMapper.Analyzers.Common.Tests;
 
 public class MapFromTests : MapBaseAnalyzerTests<MapFromAnalyzer>
 {
-    [TestCase("()\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|};")]
-    [TestCase("()\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|}\n\r.ForMember(dest => dest.UserName, opt => opt.Ignore());")]
-    [TestCase("()\n\r.ForMember(dest => dest.Name, opt => opt.Ignore())\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|};")]
+    [TestCase("()\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|};", TestName = "Only identical properties map")]
+    [TestCase("()\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|}\n\r.ForMember(dest => dest.UserName, opt => opt.Ignore());", TestName = "Identical properties map with Ignore after")]
+    [TestCase("()\n\r.ForMember(dest => dest.Name, opt => opt.Ignore())\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|};", TestName = "Identical properties map with Ignore before")]
     public async Task MapFromAnalyzerRaiseDiagnostic(string forMembers)
     {
         await VerifyExpectedAsync(MapFromAnalyzer.DiagnosticId, forMembers);
     }
 
-    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.CustomId));")]
-    [TestCase("()\n\r.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.User.Name));")]
-    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id ?? 10));")]
-    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(_ => 10));")]
-    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(new IdResolver()));")]
-    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom<IdResolver>());")]
-    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))\n\r.ForAllOtherMembers(opt => opt.Ignore());")]
+    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.CustomId));", TestName = "Different properties map")]
+    [TestCase("()\n\r.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.User.Name));", TestName = "Internal object identical properties map")]
+    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id ?? 10));", TestName = "Identical properties map with manually NullSubstitute")]
+    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(_ => 10));", TestName = "Constant map")]
+    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(new IdResolver()));", TestName = "Map by new Resolver instance")]
+    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom<IdResolver>());", TestName = "Map by generic Resolver")]
+    [TestCase("()\n\r.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))\n\r.ForAllOtherMembers(opt => opt.Ignore());", TestName = "Identical properties map with ForAllOtherMembers ignore")]
     public async Task MapFromAnalyzerSuccess(string forMembers)
     {
         var goodMapFrom = string.Format(CreateMapCode, forMembers);
         await VerifyAnalyzerAsync(goodMapFrom);
     }
     
-    [TestCase("()\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|};", "()\n;")]
-    [TestCase("()\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|}\n\r.ForMember(dest => dest.UserName, opt => opt.Ignore());", "()\n\r.ForMember(dest => dest.UserName, opt => opt.Ignore());")]
-    [TestCase("()\n\r.ForMember(dest => dest.Name, opt => opt.Ignore())\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|};", "()\n\r.ForMember(dest => dest.Name, opt => opt.Ignore())\n;")]
+    [TestCase("()\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|};", "()\n;", TestName = "Fix identical properties map when it is only it")]
+    [TestCase("()\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|}\n\r.ForMember(dest => dest.UserName, opt => opt.Ignore());", "()\n\r.ForMember(dest => dest.UserName, opt => opt.Ignore());", TestName = "Fix identical properties map with Ignore after")]
+    [TestCase("()\n\r.ForMember(dest => dest.Name, opt => opt.Ignore())\n\r.ForMember{|#0:(dest => dest.Id, opt => opt.MapFrom(src => src.Id))|};", "()\n\r.ForMember(dest => dest.Name, opt => opt.Ignore())\n;", TestName = "Fix identical properties map with Ignore before")]
     public async Task MapFromAnalyzerFixChecking(string forMembers, string forMemberFix)
     {
         await VerifyCodeFixAsync<ForMemberCodeFixProvider>(MapFromAnalyzer.DiagnosticId, forMembers, forMemberFix);
